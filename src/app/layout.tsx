f933f717299fd1119e8d1a1f594a74da05ef3c25
mpittas <webpittas@gmail.com>
2024-04-10 22:31:20 +0300

    layout change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,14 +52,16 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
-        </main>
+      <body className="antialiased mx-4 mt-8 lg:mx-auto">
+        <div className=" max-w-5xl mx-auto bg-slate-200">
+          <main className="">
+            <Navbar />
+            {children}
+            <Footer />
+            <Analytics />
+            <SpeedInsights />
+          </main>
+        </div>
       </body>
     </html>
   )
