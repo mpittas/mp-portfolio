3e3acd86a70bc25f29720a351127697f522993f7
mpittas <webpittas@gmail.com>
2024-04-10 22:37:32 +0300

    Revert to tailwind 3

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+      },
+      screens: {
+        xl: "1180px",
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
