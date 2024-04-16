fc2763746571ca67410d80f89e8131081735d1b7
mpittas <webpittas@gmail.com>
2024-04-16 18:52:59 +0200

    Test ascii 3d

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,11 @@
-import TitleBig from "@/components/widgets/TitleBig";
-import PageWrap from "@/components/layout/PageWrap";
+import TitleBig from "@/components/widgets/TitleBig"
+import PageWrap from "@/components/layout/PageWrap"
+import AsciiHolder from "@/components/ascii/AsciiHolder"
 
 export default function MainPage() {
   return (
-    <PageWrap>
-      <div className="max-w-xl">
-        <TitleBig title="About page" />
-      </div>
-    </PageWrap>
-  );
+    <div className="h-[100vh]">
+      <AsciiHolder />
+    </div>
+  )
 }
