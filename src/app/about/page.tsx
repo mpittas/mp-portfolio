b93b12a0d6310d97d9aa953c2582f7c703e88cad
mpittas <webpittas@gmail.com>
2024-04-19 16:08:57 +0300

    Working 3d example using three fiber

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,18 @@
-import TitleBig from "@/components/widgets/TitleBig";
-import PageWrap from "@/components/layout/PageWrap";
+import TitleBig from "@/components/widgets/TitleBig"
+import PageWrap from "@/components/layout/PageWrap"
+import ShoeWrap from "@/components/threede/ShoeWrap"
 
 export default function MainPage() {
   return (
-    <PageWrap>
-      <div className="max-w-xl">
-        <TitleBig title="About page" />
+    <>
+      <PageWrap>
+        <div className="max-w-xl">
+          <TitleBig title="About page" />
+        </div>
+      </PageWrap>
+      <div className="w-[100vw] h-[100vh]">
+        <ShoeWrap />
       </div>
-    </PageWrap>
-  );
+    </>
+  )
 }
