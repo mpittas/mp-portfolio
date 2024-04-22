ed5aa5b9eab2db4b0f39a93f3370503d22ab86a7
mpittas <webpittas@gmail.com>
2024-04-22 16:24:44 +0300

    Testing more

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://mp-portfolio-beige.vercel.app/",
+        port: "",
+        pathname: "/",
+      },
+    ],
+  },
+}
