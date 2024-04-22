7760374366f0a1de151d5e15d53f32780ec19c17
mpittas <webpittas@gmail.com>
2024-04-22 12:58:14 +0300

    Text text 3d effect

--- src/components/views/MainView.tsx
+++ src/components/views/MainView.tsx
@@ -4,32 +4,38 @@ import { PortfolioPosts } from "@/components/portfolio/PortfolioPosts"
 import AnimatedCircle from "@/components/anim/AnimatedCircles"
 import Image from "next/image"
 import Iframe from "react-iframe"
+import TextScene from "@/components/scenes/TextScene"
 
 export default function MainView() {
   return (
-    <PageWrap className="overflow-hidden">
-      <div className="max-w-xl py-16 pb-28 relative ">
-        <TitleBig title="Passionate front-end developer that also specializes in UI/UX." />
-        <div className="absolute top-[0%] right-[-70%]">
-          <AnimatedCircle />
-        </div>
+    <div>
+      <div className="h-[50vh]">
+        <TextScene />
       </div>
+      <PageWrap className="overflow-hidden">
+        <div className="max-w-xl mx-auto text-center mt-[-5rem] py-16 pb-28 relative ">
+          <TitleBig title="Passionate front-end developer that also specializes in UI/UX." />
+          {/* <div className="absolute top-[0%] right-[-70%]">
+            <AnimatedCircle />
+          </div> */}
+        </div>
 
-      <div className=" z-1 relative">
-        <div className="flex items-center gap-x-4 pb-4">
-          <div className=" uppercase font-bold text-neutral-400 dark:text-neutral-300">
-            Latest projects
+        <div className=" z-1 relative">
+          <div className="flex items-center gap-x-4 pb-4">
+            <div className="uppercase font-bold text-neutral-400 dark:text-neutral-300">
+              Latest projects
+            </div>
+            <Image
+              src="/arrow-double-pixel.svg"
+              width={8}
+              height={8}
+              alt="arrow-right"
+              className="opacity-70"
+            />
           </div>
-          <Image
-            src="/arrow-double-pixel.svg"
-            width={8}
-            height={8}
-            alt="arrow-right"
-            className="opacity-70"
-          />
+          <PortfolioPosts />
         </div>
-        <PortfolioPosts />
-      </div>
-    </PageWrap>
+      </PageWrap>
+    </div>
   )
 }
