f4762ff81c44121ede5ced6cc5f222097c7438f3
Marios Pittas <57490593+mpittas@users.noreply.github.com>
2024-04-23 18:07:48 +0300

    Update TextScene.js

--- src/components/anim/TextScene.js
+++ src/components/anim/TextScene.js
@@ -1,24 +1,26 @@
-"use client"
-import * as THREE from "three"
-import {Suspense, useState} from "react"
-import {Canvas, useFrame} from "@react-three/fiber"
-import {Preload, useCursor} from "@react-three/drei"
-import {Model, Fragments} from "./Text"
-import {useTheme} from "next-themes"
+
+import * as THREE from "three";
+import { Suspense, useState, useEffect } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Preload, useCursor } from "@react-three/drei";
+import { Model, Fragments } from "./Text";
+import { useTheme } from "next-themes";
 
 function Scene() {
-  const {theme, setTheme} = useTheme()
-  const vec = new THREE.Vector3()
-  const [clicked, setClicked] = useState(false)
-  const [hovered, setHovered] = useState(false)
-  useCursor(hovered)
+  const { theme, setTheme } = useTheme();
+  const vec = new THREE.Vector3();
+  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  useCursor(hovered);
+
   useFrame((state) => {
     state.camera.position.lerp(
       vec.set(clicked ? -10 : 0, clicked ? 10 : 0, 20),
       0.1
-    )
-    state.camera.lookAt(0, 0, 0)
-  })
+    );
+    state.camera.lookAt(0, 0, 0);
+  });
+
   return (
     <group>
       <Fragments visible={clicked} />
@@ -29,15 +31,27 @@ function Scene() {
         onPointerOut={() => setHovered(false)}
       />
     </group>
-  )
+  );
 }
 
 export default function TextScene() {
-  const {theme, setTheme} = useTheme()
-  const intensity = theme === "dark" ? 20 : 3
+  const { theme, setTheme } = useTheme();
+  const intensity = theme === "dark" ? 20 : 3;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640); // Tailwind's default breakpoint for 'sm'
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <Canvas dpr={[1, 2]} orthographic camera={{zoom: 260}}>
+    <Canvas dpr={[1, 2]} orthographic camera={{ zoom: isMobile ? 100 : 260 }}>
       <directionalLight
         position={[0, 3, 5]}
         color="#ffffff"
@@ -48,5 +62,5 @@ export default function TextScene() {
         <Preload all />
       </Suspense>
     </Canvas>
-  )
+  );
 }
